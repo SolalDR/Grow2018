1d287d4a7f6bf00be3c44e4ab9aad15d3d9022b5
SolalDR <solal.dussout-revel@hotmail.fr>
2018-10-03 20:43:45 +0200

    Improve code quality in helpers

--- app/scripts/helpers/Clock.js
+++ app/scripts/helpers/Clock.js
@@ -1,53 +1,54 @@
-/*
-*
-*	Based on THREE.Clock
-* 	https://github.com/mrdoob/three.js/blob/master/src/core/Clock.js
-*
-*/
-
+/**
+ * @class Create a global timer to manage animation
+ * @author SolalDR http://solaldussout-revel.com
+ * Based on THREE.Clock https://github.com/mrdoob/three.js/blob/master/src/core/Clock.js
+ */
 export default class Clock {
-	
-	constructor ( autoStart ) {
 
-		this.autoStart = ( autoStart !== undefined ) ? autoStart : true;
-		this._start = 0; 
-		
-		this.old = 0; 
-		this.running = false; 
-		this.elapsed = 0; 
 
-		if ( this.autoStart ) {
-			this.start();
-		}
+  /**
+   * @constructor
+   * @param  {boolean} autoStart If true, clock will start immediatly
+   */
+	constructor ( autoStart = true ) {
+		this.autoStart = autoStart;
+		this._start = 0;
 
+		this.old = 0;
+		this.running = false;
+		this.elapsed = 0;
+
+		if ( this.autoStart ) this.start();
 	}
 
+
+  /**
+   * Start/Restart the timer
+   */
 	start () {
-		
 		this._start = Date.now();
 		this.old = this._start;
 		this.elapsed = 0;
-		this.running = true; 
+		this.running = true;
+	}
 
-	} 
 
+  /**
+   * Pause the timer
+   */
 	stop () {
-
 		this.running = false;
-
 	}
 
+
+  /**
+   * Update timer from last time, you need to run this method on the render
+   */
 	update () {
-
 		if( this.running ){
-
 			var now = Date.now();
-
-			this.elapsed += now - this.old; 
+			this.elapsed += now - this.old;
 			this.old = now;
-
 		}
-		
 	}
-
 }
