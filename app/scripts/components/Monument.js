b6511ced95d4961f2d39a707bc00d1308af04f6a
Julien Dargelos <julien.dargelos@me.com>
2018-10-25 18:57:00 +0200

    Monuments

--- app/scripts/components/Monument.js
+++ app/scripts/components/Monument.js
@@ -1,9 +1,13 @@
 import OBJLoader from "../helpers/OBJLoader.js";
+import config from "../config.js";
+import refMarkersDatas from "./../../datas/refMarkers.json";
 
 class Monument {
-  constructor({slug, position}) {
+  constructor({slug, position, rotation, scale}) {
     this.slug = slug;
     this.position = position;
+    this.rotation = rotation;
+    this.scale = scale;
     this.object = null;
   }
 
@@ -25,12 +29,25 @@ class Monument {
     this.updateObject();
   }
 
+  get scale() {
+    return this._scale;
+  }
+
+  set scale(v) {
+    this._scale = v;
+    this.updateObject();
+  }
+
   updateObject() {
     if(!this.object) return;
-    this.object.position.x = this.position.x;
-    this.object.position.y = this.position.y;
-    this.object.position.z = this.position.z;
-    this.object.scale.copy(this.constructor.scale);
+    this.object.position.copy(this.position);
+    this.object.rotation.set(
+      this.rotation.x,
+      this.rotation.y,
+      this.rotation.z
+    );
+    this.object.scale.set(this.scale, this.scale, this.scale);
+    this.object.name = this.slug;
   }
 
   load() {
@@ -41,9 +58,55 @@ class Monument {
       }, null, reject);
     });
   }
+
+  // TODO: remove
+
+  lookAtItFrom(camera, y = 150, radius = 400, speed = 0.01) {
+    camera.position.y = this.position.y + y;
+
+    var alpha = 0;
+    var animationFrame;
+
+    var update = () => {
+      camera.position.x = this.position.x + Math.cos(alpha)*radius;
+      camera.position.z = this.position.z + Math.sin(alpha)*radius;
+      camera.lookAt(this.object.position);
+      alpha += speed;
+
+      animationFrame = requestAnimationFrame(update);
+    };
+
+    var stop = () => {
+      cancelAnimationFrame(animationFrame);
+      window.removeEventListener('mousedown', stop);
+    };
+
+    window.addEventListener('mousedown', stop);
+
+    update();
+  }
+
+  static convert(latitude, longitude) {
+    var x = THREE.Math.mapLinear(
+      latitude,
+      refMarkersDatas.bottomRight.coords.latitude,
+      refMarkersDatas.topLeft.coords.latitude,
+      config.markers.refs.bottomRight.x,
+      config.markers.refs.topLeft.x
+    );
+
+    var y = THREE.Math.mapLinear(
+      longitude,
+      refMarkersDatas.bottomRight.coords.longitude,
+      refMarkersDatas.topLeft.coords.longitude,
+      config.markers.refs.bottomRight.z,
+      config.markers.refs.topLeft.z
+    );
+
+    console.log(`"position": {"x": ${x}, "y": 0, "z": ${y}},`)
+  }
 }
 
 Monument.loader = new OBJLoader();
-Monument.scale = new THREE.Vector3(0.1, 0.1, 0.1);
 
 export default Monument;
